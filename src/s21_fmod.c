715c3acb6f6c9de874ea745190dfb068b3d6555b
Ariane Pott <arianepo@at-m6.msk.21-school.ru>
2022-11-17 18:09:00 +0300

    hello world

--- src/s21_fmod.c
+++ src/s21_fmod.c
@@ -1,7 +1,7 @@
 #include "s21_math.h"
 
 long double s21_fmod(double x, double y) {
-  long double res = s21_fabs(x), tmpy = s21_fabs(y), tmpx = res;
-  for (int i = 1; res > tmpy; i++) res = tmpx - tmpy * i;
+  long double res = x == y ? 0 : s21_fabs(x), tmpy = s21_fabs(y);
+  while (res > tmpy) res -= tmpy;
   return x < 0 ? -res : res;
 }
